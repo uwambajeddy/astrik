8628aaca3bb88d796cbe3a607936e9fde90aaf26
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2023-04-11 14:11:07 +0200

    some fixes

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -53,7 +53,7 @@ export const createProject = catchAsync(async (req, res, next) => {
   if (req.file) {
     req.body.image = await fileUpload(req);
   } else {
-    req.body.image = "https://res.cloudinary.com/dvibmdi1y/image/upload/v1681210810/astrik/images/background/aene4yzyd6ptswi4mx7u_hvamjd.jpg"
+    req.body.image = "https://res.cloudinary.com/dvibmdi1y/image/upload/v1681214827/astrik/images/background/aene4yzyd6ptswi4mx7u_n14bcd.jpg"
   }
 
   const projectImage = await projectImageModal.create({ project: project._id, image: req.body.image });
